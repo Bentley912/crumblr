27048306d6041df9b09b2b5c0e64e748de7b0c62
Patrick Bentley <pbentley07@gmail.com>
2016-08-04 00:20:06 -0400

    Created Posts Controller and Model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+
+
+  def index
+  end
+
+  def new
+
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+end
