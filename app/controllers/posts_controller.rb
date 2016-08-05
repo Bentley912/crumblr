433a83dff4b34e13d91767b8b48720ca826ac77d
Patrick Bentley <pbentley07@gmail.com>
2016-08-04 20:27:01 -0400

    Added Post Index Controller Method and View

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
 
 
   def index
+    @posts=Post.all.order('created_at DESC')
   end
 
   def new
@@ -11,7 +12,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.save
-    
+
     redirect_to @post
   end
 
